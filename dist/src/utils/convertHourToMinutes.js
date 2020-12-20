27d63cbcc1f446304f9331b20dee08287e8aa2e2
victorsoares96 <vitorsoares96@hotmail.com>
2020-12-20 10:14:12 -0300

    delete readme

--- dist/src/utils/convertHourToMinutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function convertHourToMinutes(time) {
-    const [hour, minutes] = time.split(':').map(Number);
-    const timeInMinutes = (hour * 60) + minutes;
-    return timeInMinutes;
-}
-exports.default = convertHourToMinutes;
